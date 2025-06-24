17a7b76c95c51479958915d2cc6adcbdd20ec1bf
Jeet Chatterjee <149117511+itsmecaliber@users.noreply.github.com>
2025-06-24 19:41:30 +0530

    Update UserSearchBar.jsx

--- frontend/src/components/UserSearchBar.jsx
+++ frontend/src/components/UserSearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = "http://localhost:8080/api/profile/search";
+const BACKEND_URL = "https://scod.onrender.com/api/profile/search";
 
 const UserSearchBar = () => {
   const [query, setQuery] = useState("");
